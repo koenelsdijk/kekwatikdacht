956cd949ec79db015bc47b239f4d2e839de889ef
Koen Elsdijk - Kantabuth <k.elsdijk@datanext.nl>
2025-06-09 20:57:12 +0200

    WIP prettify html

--- GhostBackupParser/GhostBackupParser/StartCommand.cs
+++ GhostBackupParser/GhostBackupParser/StartCommand.cs
@@ -1,6 +1,9 @@
 using System.Text.Json;
+using System.Xml;
+using System.Xml.Linq;
 using GhostBackupParser;
 using Spectre.Console.Cli;
+using HtmlAgilityPack;
 
 public class StartCommand : AsyncCommand<StartCommand.Settings>
 {
@@ -17,32 +20,41 @@ public class StartCommand : AsyncCommand<StartCommand.Settings>
 
   public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
   {
-    var isJson = IsJson(settings.BackupJson);
-    var json = isJson 
-      ? settings.BackupJson 
-      : File.Exists(settings.BackupJson) 
-        ? File.ReadAllText(settings.BackupJson) 
-        : null;
-    if (json == null) 
-      return 1;
-    
-    var parsedJson = JsonSerializer.Deserialize<GhostBackupFormat>(json, SerializerOptions);
-
-    if (parsedJson?.Db is not { Length: > 0 }) 
-      return 1;
-    
-    foreach (var post in parsedJson.Db[0].Data.Posts)
+    try
     {
-      var filename = post.Status == "draft" ? $"{post.Slug}_DRAFT.html" : $"{post.Slug}.html";
-      var dir = $"{post.Type}s"; // Create plural by appending 's'
-      if(!string.IsNullOrWhiteSpace(settings.ContentRoot)) dir = Path.Combine(settings.ContentRoot, dir);
+      var isJson = IsJson(settings.BackupJson);
+      var json = isJson 
+        ? settings.BackupJson 
+        : File.Exists(settings.BackupJson) 
+          ? File.ReadAllText(settings.BackupJson) 
+          : null;
+      if (json == null) 
+        return 1;
+    
+      var parsedJson = JsonSerializer.Deserialize<GhostBackupFormat>(json, SerializerOptions);
 
-      Directory.CreateDirectory(dir);
-      await File.WriteAllTextAsync($"{dir}/{filename}", post.Html);
+      if (parsedJson?.Db is not { Length: > 0 }) 
+        return 1;
+    
+      foreach (var post in parsedJson.Db[0].Data.Posts)
+      {
+        var filename = post.Status == "draft" ? $"{post.Slug}_DRAFT.html" : $"{post.Slug}.html";
+        var dir = $"{post.Type}s"; // Create plural by appending 's'
+        if(!string.IsNullOrWhiteSpace(settings.ContentRoot)) dir = Path.Combine(settings.ContentRoot, dir);
 
-      Console.WriteLine($"Wrote to {dir}/{filename}");
+        Directory.CreateDirectory(dir);
+        var prettyHtml = PrettifyXml(post.Html);
+        await File.WriteAllTextAsync($"{dir}/{filename}", prettyHtml);
+
+        Console.WriteLine($"Wrote to {dir}/{filename}");
+      }
+      return 0;
     }
-    return 0;
+    catch (Exception e)
+    {
+      Console.WriteLine(e);
+      throw;
+    }
   }
 
   private static bool IsJson(string s)
@@ -56,5 +68,26 @@ public class StartCommand : AsyncCommand<StartCommand.Settings>
     {
       return false;
     }
+  }
+
+  private static string PrettifyXml(string contentHtml)
+  {
+    var doc = new HtmlDocument();
+    contentHtml = $"<body>{contentHtml}</body>";
+    doc.LoadHtml(contentHtml);
+
+    var xDoc = new XDocument(new XElement("html", XElement.Parse(doc.DocumentNode.OuterHtml
+      .Replace("<hr>", "<hr/>")
+      .Replace("<br>", "<br/>"))));
+    var settings = new XmlWriterSettings { 
+      Indent = true, 
+      NewLineOnAttributes = true, 
+      OmitXmlDeclaration = true,
+      NewLineChars = "\r\n" 
+    };
+    using var writer = new StringWriter();
+    using var xmlWriter = XmlWriter.Create(writer, settings);
+    xDoc.Save(xmlWriter);
+    return xDoc.ToString();
   }
 }
